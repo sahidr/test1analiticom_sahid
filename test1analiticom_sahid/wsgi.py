e77d12f771669a0a9ea9a29ebd2999ebb19b2710
sahidr <sahidr93@gmail.com>
2017-04-06 10:33:54 -0400

    Base for user login/signup app

---
+++ test1analiticom_sahid/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for test1analiticom_sahid project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "test1analiticom_sahid.settings")
+
+application = get_wsgi_application()
